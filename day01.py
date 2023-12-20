f9a9f8022d0c22bef8252e3d91490ddec4df5184
Blair Frandeen <blairfrandeen@gmail.com>
2023-12-20 15:40:25 -0800

    day 1 was harder than it looked

---
+++ day01.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+
+def first_digit(word: str) -> Optional[int]:
+    for char in word:
+        if char.isdigit():
+            return int(char)
+
+
+def cal_value(line: str) -> int:
+    return first_digit(line) * 10 + first_digit(line[::-1])
+
+
+def part_1(puzzle_input: str) -> int:
+    return sum(map(cal_value, puzzle_input.splitlines()))
+
+
+SPELLED_DIGITS = dict(
+    zip("one two three four five six seven eight nine".split(), range(1, 10))
+)
+
+
+def replace_word(line: str) -> str:
+    matches = []
+    for number, digit in SPELLED_DIGITS.items():
+        number_matches = findall(line, number)
+        for match in number_matches:
+            matches.append((match, str(digit)))
+        digit_matches = findall(line, str(digit))
+        for match in digit_matches:
+            matches.append((match, str(digit)))
+    matches = sorted([m for m in matches if m[0] >= 0])
+    return "".join([m[1] for m in matches])
+
+
+def findall(line: str, substr: str) -> list[int]:
+    start_index = 0
+    results = []
+    while True:
+        res = line.find(substr, start_index)
+        if res < 0:
+            break
+        start_index = res + 1
+        results.append(res)
+    return results
+
+
+def part_2(puzzle_input: str) -> int:
+    return sum(map(cal_value, map(replace_word, puzzle_input.splitlines())))
---
+++ tests/test_1.py
@@ -0,0 +1,69 @@
+import pytest
+
+import day01
+
+
+@pytest.mark.parametrize("word, expected", [("abc123", 1)])
+def test_first_digit(word, expected):
+    assert day01.first_digit(word) == expected
+
+
+@pytest.mark.parametrize("line, expected", [("abc123", 13)])
+def test_cal_value(line, expected):
+    assert day01.cal_value(line) == expected
+
+
+SAMPLE_INPUT = """\
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+"""
+
+
+def test_sample_1():
+    assert day01.part_1(SAMPLE_INPUT) == 142
+
+
+@pytest.mark.parametrize(
+    "line, expected",
+    [
+        ("two1nine", "219"),
+        ("eightwothree", "823"),
+        ("abcone2threexyz", "123"),
+        ("xtwone3four", "2134"),
+        ("4nineeightseven2", "49872"),
+        ("fivefiveeight99", "55899"),
+        ("zoneight234", "18234"),
+        ("7pqrstsixteen", "76"),
+    ],
+)
+def test_replace_word(line, expected):
+    assert day01.replace_word(line) == expected
+
+
+SAMPLE_INPUT_2 = """\
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"""
+
+
+def test_sample_2():
+    assert day01.part_2(SAMPLE_INPUT_2) == 281
+
+
+@pytest.mark.parametrize(
+    "line, substr, expected",
+    [
+        ("fivefive3eight", "five", [0, 4]),
+        ("nine999", "9", [4, 5, 6]),
+        ("dollars", "sense", []),
+    ],
+)
+def test_findall(line, substr, expected):
+    assert day01.findall(line, substr) == expected
