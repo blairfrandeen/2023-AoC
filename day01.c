bc887e4865a8194b8909864113d9e8d60617de21
Blair Frandeen <blairfrandeen@gmail.com>
2023-12-20 22:02:14 -0800

    add better benchmarking

--- day01.c
+++ day01.c
@@ -1,40 +1,27 @@
 #include <errno.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-// benchmarking macros
-#define CLOCK_INIT clock_t start_time, end_time;
-#define CLOCK_START start_time = clock();
-#define CLOCK_END end_time = clock(); printf("Executed in %.6f ms\n", (float)(end_time - start_time)/CLOCKS_PER_SEC*1000);
-
 /* Determine if a character is a digit. Return that character
  * as an integer if so, otherwise return -1 */
-int is_digit(char chr) {
-	int res = chr - '0';
+int8_t is_digit(char chr) {
+	int8_t res = chr - '0';
 	if(res <= 9) {
 		return res;
 	}
 	return -1;
 }
 
-int main(int argc, char *argv[]) {
-	CLOCK_INIT
-
-	char* datafile = "inputs/01";
-	FILE *file_stream = fopen(datafile, "r");
-	if (file_stream == NULL) {
-		printf("Error: could not open file %s: %s.\n", datafile, strerror(errno));
-		exit(-1);
-	}
-
-	CLOCK_START
+int part_1(FILE *file_stream) {
 	char current_chr;   // current character we're reading
 	int result = 0; // final result we'll return
-	int first_digit = 0;  // flag to indicate that we've found the first digit in a line
-	int second_digit;
-	int current_digit;
+	bool first_digit = 0;  // flag to indicate that we've found the first digit in a line
+	uint8_t second_digit;
+	int8_t current_digit;
 	
 	// read every byte in the file
 	while((current_chr = fgetc(file_stream)) != EOF) {
@@ -57,9 +44,31 @@ int main(int argc, char *argv[]) {
 			first_digit = 0;
 		}
 	}
-	
-		
-	CLOCK_END
+	return result;
+}
+
+int main(int argc, char *argv[]) {
+	char* datafile = "inputs/01";
+	FILE *file_stream = fopen(datafile, "r");
+	if (file_stream == NULL) {
+		printf("Error: could not open file %s: %s.\n", datafile, strerror(errno));
+		exit(-1);
+	}
+
+	int num_runs = 1000;
+	float total_time = 0.0;
+	int result;
+
+	clock_t start_time, end_time;
+	for(int i=0; i < num_runs; i++) {
+		start_time = clock();
+		result = part_1(file_stream);
+		end_time = clock();
+		rewind(file_stream);
+		total_time += (float)(end_time - start_time) / CLOCKS_PER_SEC;
+	}
+	float avg_time = total_time / num_runs;
+	printf("Average Runtime: %.3f ms (%d runs)\n", avg_time * 1000, num_runs);
 	fclose(file_stream);
 	printf("Part 1: %d\n", result);
 	return 0;
