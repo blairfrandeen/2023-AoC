1f86a61ec0625a9843ebc8b8b96352850d1e55bf
Blair Frandeen <blairfrandeen@gmail.com>
2023-12-21 16:10:27 -0800

    part 1 works with C extension! and is too slow

--- aocmodule.c
+++ aocmodule.c
@@ -1,13 +1,56 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <stdbool.h>
+
+/* Determine if a character is a digit. Return that character
+ * as an integer if so, otherwise return -1 */
+int8_t is_digit(char chr) {
+    int8_t res = chr - '0';
+    if(res <= 9) {
+        return res;
+    }
+    return -1;
+}
 
 static PyObject *
 aoc_part1(PyObject *self, PyObject *args)
 {
-    // for now, don't take any args, and just return
-    // everyone's favorite integer
-    return PyLong_FromLong(42);
+    const char *puzzle_input;
+
+    if (!PyArg_ParseTuple(args, "s", &puzzle_input))
+        return NULL;
+
+    int result = 0; // final result we'll return
+    bool first_digit = 0;  // flag to indicate that we've found the first digit in a line
+    uint8_t second_digit = 0;
+    int8_t current_digit;
+    
+    // read every byte in the file
+    for(size_t i=0; i < strlen(puzzle_input); i++) {
+        if((current_digit = is_digit(puzzle_input[i])) > 0) {
+            // if we haven't yet found the first digit in a line, set the first_digit
+            // flag to indicate that we have, and add 10 times that digit to the result
+            if (!first_digit) {
+                first_digit = 1;
+                result += current_digit * 10;
+            }
+            // set the second digit to the current digit regardless; we'll only add it
+            // to the result once we know it's the last digit in the line. This also
+            // covers the case where a line has only one digit, e.g. "se7en" should yield 77.
+            second_digit = current_digit;
+        }
+        // if we're at the end of a line, add the second digit, and reset the 
+        // flag to indicate we haven't found the first digit
+        if(puzzle_input[i] == '\n') {
+            result += second_digit;
+            first_digit = 0;
+            second_digit = 0;
+        }
+    }
+    result += second_digit; // allow for input without trailing newline
+    return PyLong_FromLong(result);
 }
+
 
 // these are the functions available from this module
 static PyMethodDef aocMethods[] = {
