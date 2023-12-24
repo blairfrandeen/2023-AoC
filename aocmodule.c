84dd8bd77844101a57c8302e41baccfb4b20f8b7
Blair Frandeen <blairfrandeen@gmail.com>
2023-12-23 20:04:12 -0800

    formatting in aocmodule

--- aocmodule.c
+++ aocmodule.c
@@ -4,9 +4,11 @@
 
 /* Determine if a character is a digit. Return that character
  * as an integer if so, otherwise return -1 */
-int8_t is_digit(char chr) {
+int8_t
+_is_digit(char chr)
+{
     int8_t res = chr - '0';
-    if(res <= 9) {
+    if (res <= 9) {
         return res;
     }
     return -1;
@@ -20,36 +22,38 @@ aoc_part1(PyObject *self, PyObject *args)
     if (!PyArg_ParseTuple(args, "s*", &puzzle_input))
         return NULL;
 
-    int result = 0; // final result we'll return
-    bool first_digit = 0;  // flag to indicate that we've found the first digit in a line
+    int result = 0;  // final result we'll return
+    // flag to indicate that we've found the first digit in a line
+    bool first_digit = 0;
     uint8_t second_digit = 0;
     int8_t current_digit;
-    char *current_chr = (char*)puzzle_input.buf;
-    
+    char *current_chr = (char *)puzzle_input.buf;
+
     // read every byte in the buffer
-    for(Py_ssize_t i=0; i < puzzle_input.len; i++) {
-        if((current_digit = is_digit(*current_chr)) > 0) {
-            // if we haven't yet found the first digit in a line, set the first_digit
-            // flag to indicate that we have, and add 10 times that digit to the result
+    for (Py_ssize_t i = 0; i < puzzle_input.len; i++) {
+        if ((current_digit = _is_digit(*current_chr)) > 0) {
+            // if we haven't yet found the first digit in a line, set the
+            // first_digit flag to indicate that we have, and add 10 times that
+            // digit to the result
             if (!first_digit) {
                 first_digit = 1;
                 result += current_digit * 10;
             }
-            // set the second digit to the current digit regardless; we'll only add it
-            // to the result once we know it's the last digit in the line. This also
-            // covers the case where a line has only one digit, e.g. "se7en" should yield 77.
+            // to the result once we know it's the last digit in the line. This
+            // also covers the case where a line has only one digit, e.g.
+            // "se7en" should yield 77.
             second_digit = current_digit;
         }
-        // if we're at the end of a line, add the second digit, and reset the 
+        // if we're at the end of a line, add the second digit, and reset the
         // flag to indicate we haven't found the first digit
-        if(*current_chr == '\n') {
+        if (*current_chr == '\n') {
             result += second_digit;
             first_digit = 0;
             second_digit = 0;
         }
         current_chr++;
     }
-    result += second_digit; // allow for input without trailing newline
+    result += second_digit;  // allow for input without trailing newline
 
     // release the buffer
     PyBuffer_Release(&puzzle_input);
@@ -57,24 +61,20 @@ aoc_part1(PyObject *self, PyObject *args)
     return PyLong_FromLong(result);
 }
 
-
 // these are the functions available from this module
 static PyMethodDef aocMethods[] = {
     {"part1", aoc_part1, METH_VARARGS, "solve part 1"},
-    {NULL, NULL, 0, NULL} // sentinel
+    {NULL, NULL, 0, NULL}  // sentinel
 };
 
 // this is the module definition
-static struct PyModuleDef aocmodule = {
-    PyModuleDef_HEAD_INIT,
-    "aoc", // module name
-    NULL, // documentation, nothing for now
-    -1,
-    aocMethods
-};
+static struct PyModuleDef aocmodule = {PyModuleDef_HEAD_INIT,
+                                       "aoc",  // module name
+                                       NULL,  // documentation, nothing for now
+                                       -1, aocMethods};
 
 // this creates a new module and allows it to be imported
-PyMODINIT_FUNC 
+PyMODINIT_FUNC
 PyInit_aocmodule(void)
 {
     return PyModule_Create(&aocmodule);
