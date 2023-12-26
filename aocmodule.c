0274debb44b760d27632e156a1d6a8224652a8bb
Blair Frandeen <blairfrandeen@gmail.com>
2023-12-26 06:38:54 -0800

    day 1 part 2 as extension module

--- aocmodule.c
+++ aocmodule.c
@@ -61,9 +61,94 @@ aoc_part1(PyObject *self, PyObject *args)
     return PyLong_FromLong(result);
 }
 
+static PyObject *
+aoc_part2(PyObject *self, PyObject *args)
+{
+    Py_buffer puzzle_input;
+
+    if (!PyArg_ParseTuple(args, "s*", &puzzle_input))
+        return NULL;
+
+    int result = 0;  // final result we'll return
+    // flag to indicate that we've found the first digit in a line
+    bool first_digit = 0;
+    uint8_t second_digit = 0;
+    int8_t current_digit;
+
+    bool match = 0;
+    char *current_chr = (char *)puzzle_input.buf;
+    char *next_match;
+
+    // hard-code the numbers we're looking for
+    const char NUMS[9][6] = {"one", "two",   "three", "four", "five",
+                             "six", "seven", "eight", "nine"};
+
+    // read every byte in the file
+    for (Py_ssize_t i = 0; i < puzzle_input.len; i++) {
+        if ((current_digit = _is_digit(*current_chr)) > 0) {
+            if (!first_digit) {
+                first_digit = 1;
+                result += current_digit * 10;
+                /* printf("a: %d\n", result); */
+            }
+            /* printf("D: %d - %d\n", current_digit, result); */
+            second_digit = current_digit;
+        }
+        else {
+            for (int j = 0; j < 9; j++) {  // loop through the possible words
+                // for each word, go through letter by letter and see
+                // if the word matches
+                for (size_t k = 0; k < strlen(NUMS[j]); k++) {
+                    // check that we're not at the end of the input
+                    if ((Py_ssize_t)k + i >= puzzle_input.len) {
+                        match = 0;
+                        break;
+                    }
+                    // point to the kth position forward in the buffer
+                    next_match = current_chr + k;
+
+                    if (*next_match != NUMS[j][k]) {
+                        match = 0;
+                        break;
+                    }
+                    /* printf("%c", *next_match); */
+                    // if we get to the end of the loop, we found a match.
+                    match = 1;
+                }
+                if (match) {
+                    current_digit = j + 1;
+                    if (!first_digit) {
+                        first_digit = 1;
+                        result += current_digit * 10;
+                        /* printf("b: %d\n", result); */
+                    }
+                    second_digit = current_digit;
+                    /* printf("W: %d - %d\n", current_digit, result); */
+                    break;
+                }
+            }
+        }
+        if (*current_chr == '\n') {
+            result += second_digit;
+            /* printf("c: %d\n", result); */
+            first_digit = 0;
+            match = 0;
+        }
+        current_chr++;
+    }
+    if (*(current_chr - 1) != '\n') {
+        /* printf("errs: %c\n", *(current_chr - 1)); */
+        result += second_digit;  // allow for input without trailing newline
+    }
+    // release the buffer
+    PyBuffer_Release(&puzzle_input);
+
+    return PyLong_FromLong(result);
+}
 // these are the functions available from this module
 static PyMethodDef aocMethods[] = {
     {"part1", aoc_part1, METH_VARARGS, "solve part 1"},
+    {"part2", aoc_part2, METH_VARARGS, "solve part 2"},
     {NULL, NULL, 0, NULL}  // sentinel
 };
 
