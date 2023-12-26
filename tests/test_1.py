4cf3d551bf03a470830ea6a5af527e8d1163859b
Blair Frandeen <blairfrandeen@gmail.com>
2023-12-26 07:06:39 -0800

    tests: add pytest-benchmarks and input fixture

--- tests/test_1.py
+++ tests/test_1.py
@@ -2,7 +2,32 @@ import pytest
 
 import day01
 
-from aocmodule import part1
+from aocmodule import part1, part2
+
+
+@pytest.fixture
+def puzzle_input():
+    with open("inputs/01", "r") as input_file:
+        puzzle_input = input_file.read()
+    return puzzle_input
+
+
+class TestBenchmarks:
+    def test_part_1_extension(self, benchmark, puzzle_input):
+        result = benchmark(part1, puzzle_input)
+        assert result == 54953
+
+    def test_part_1_python(self, benchmark, puzzle_input):
+        result = benchmark(day01.part_1, puzzle_input)
+        assert result == 54953
+
+    def test_part_2_extension(self, benchmark, puzzle_input):
+        result = benchmark(part2, puzzle_input)
+        assert result == 53868
+
+    def test_part_2_python(self, benchmark, puzzle_input):
+        result = benchmark(day01.part_2, puzzle_input)
+        assert result == 53868
 
 
 @pytest.mark.parametrize("word, expected", [("abc123", 1)])
@@ -39,7 +64,7 @@ def test_sample_1():
     assert day01.part_1(SAMPLE_INPUT) == 142
 
 
-def test_sample_ext():
+def test_sample1_ext():
     assert part1(SAMPLE_INPUT) == 142
 
 
@@ -73,6 +98,10 @@ zoneight234
 
 def test_sample_2():
     assert day01.part_2(SAMPLE_INPUT_2) == 281
+
+
+def test_sample2_ext():
+    assert part2(SAMPLE_INPUT_2) == 281
 
 
 @pytest.mark.parametrize(
